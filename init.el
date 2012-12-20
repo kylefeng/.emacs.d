aa99f9ae729c86632ae3e2e095e256f7f1c5c257
kylefeng <kylefengg@gmail.com>
2012-12-20 14:22:40 +0800

    Add html mode hook

--- init.el
+++ init.el
@@ -193,3 +193,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; CoffeeScript
 (custom-set-variables '(coffee-tab-width 2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; HTML mode
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            (setq sgml-basic-offset 4)
+            (setq indent-tabs-mode t)))
