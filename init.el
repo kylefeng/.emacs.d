6c1cb27668a76c6b1ecdb222178569c84569e1e3
kylefeng <kylefengg@gmail.com>
2014-03-03 00:25:40 +0800

    add nrepl auto-complete

--- init.el
+++ init.el
@@ -138,6 +138,7 @@
     clojure-mode
     clojure-test-mode
     cider
+    ac-nrepl
     coffee-mode
     erlang
     auto-complete
@@ -207,6 +208,23 @@
 
 ;; Make C-c C-z switch to the CIDER REPL buffer in the current window:
 (setq cider-repl-display-in-current-window nil)
+
+;; ac-nrepl
+(require 'ac-nrepl)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; CoffeeScript
 
