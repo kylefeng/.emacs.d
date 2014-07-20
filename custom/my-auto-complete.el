b31b9c7f91ab5a77fe5dc64a8ea1ce00992c3fc4
kylefeng <kylefengg@gmail.com>
2014-07-20 16:04:03 +0800

    Change package management to Pallet

--- custom/my-auto-complete.el
+++ custom/my-auto-complete.el
@@ -1,6 +1,7 @@
 ;; auto-complete
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4/dict/")
+(add-to-list 'ac-dictionary-directories
+             "~/.emacs.d/.cask/24.3.1/elpa/auto-complete-20140618.2217/dict")
 (ac-config-default)
 
 (add-hook 'text-mode-hook
--- init.el
+++ init.el
@@ -1,43 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Package
 
-;; Marmalade
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
-
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar my-packages 
-  '(starter-kit
-    starter-kit-lisp
-    starter-kit-bindings
-    starter-kit-eshell
-    clojure-mode
-    clojure-test-mode
-    cider
-    ac-nrepl
-    coffee-mode
-    erlang
-    auto-complete
-    markdown-mode
-    highlight-parentheses
-    yaml-mode
-    vline
-    hl-line+
-    col-highlight
-    crosshairs
-    haskell-mode
-    soft-morning-theme
-    json-mode)
-  "A list of packages to ensure are installed at launch.")
-
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+;; Cask
+(require 'cask "/usr/local/Cellar/cask/0.7.0/cask.el")
+(cask-initialize)
+(require 'pallet)
 
 ;; load custom
 (add-to-list 'load-path "~/.emacs.d/custom")
